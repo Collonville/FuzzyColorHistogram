88d4fe1ffedf8e3f15934cd7bd48e60eae1ddfaf
Collonville <ht235711@yahoo.co.jp>
2016-07-08 18:43:08 +0900

    Add histogram, button

--- FuzzyColorHistogram1/MainWindow.xaml.cs
+++ FuzzyColorHistogram1/MainWindow.xaml.cs
@@ -23,7 +23,11 @@ namespace FuzzyColorHistogram1
     {
         private List<string> dateSetPath = new List<string>();
 
-        private const int Division = 8; 
+        private List<Vector<double>> points = new List<Vector<double>>();
+        private Vector<double> rgbHist;
+        private Vector<double> labHist;
+
+        private int Division = 4; 
 
         private string[] _supportExts = { ".bmp", ".jpg", ".jpeg", ".png", ".tif", ".tiff" };
         /// <summary>
@@ -34,7 +38,9 @@ namespace FuzzyColorHistogram1
             get { return _supportExts; }
         }
 
-        public PlotModel HistModel { get; private set; }
+        public PlotModel ColorHist  { get; private set; }
+        public PlotModel LabHist { get; private set; }
+        public PlotModel FCHHist { get; private set; }
 
         /// <summary>
         /// 表示する画像を取得または設定します
@@ -53,7 +59,11 @@ namespace FuzzyColorHistogram1
         {
             this.DataContext = this;
 
-            HistModel = new PlotModel();
+            ColorHist = new PlotModel();
+            LabHist = new PlotModel();
+            FCHHist = new PlotModel();
+
+            initGraph();
 
             //データセットを開く
             //DataSet.openAllDataSet(ref dateSetPath);
@@ -78,10 +88,8 @@ namespace FuzzyColorHistogram1
             }
         }
 
-        private List<Vector<double>> getColorData()
+        private void getColorData(ref List<Vector<double>> points)
         {
-            List<Vector<double>> points = new List<Vector<double>>();
-
             for (int y = 0; y < ImageSource.PixelHeight; y++)
             {
                 for (int x = 0; x < ImageSource.PixelWidth; x++)
@@ -90,10 +98,23 @@ namespace FuzzyColorHistogram1
                     points.Add(new DenseVector(new double[] { color.R, color.G, color.B }));
                 }
             }
-
-            return points;
         }
 
+
+        private void initGraph()
+        {
+            ColorHist.Series.Clear();
+            ColorHist.InvalidatePlot(true);
+            ColorHist.Title = "RGB Histogram";
+
+            LabHist.Series.Clear();
+            LabHist.InvalidatePlot(true);
+            LabHist.Title = "Lab Histogram";
+
+            FCHHist.Series.Clear();
+            FCHHist.InvalidatePlot(true);
+            FCHHist.Title = "Fuzzy Color Histogram";
+        }
         private Vector<double> createRGBHist(List<Vector<double>> rgbList)
         {
             Vector<double> RGB = new DenseVector((int)Math.Pow(Division, 3.0));
@@ -104,6 +125,23 @@ namespace FuzzyColorHistogram1
 
                 RGB[index]++;
             }
+
+            // グラフの作成
+            var graphStep = new StairStepSeries()
+            {
+                Color = OxyColors.Red,
+                VerticalLineStyle = LineStyle.None,
+                MarkerType = MarkerType.None
+            };
+
+            for (int x = 0; x < RGB.Count(); x++)
+            {
+                graphStep.Points.Add(new DataPoint(x, RGB[x]));
+            }
+
+            ColorHist.Series.Clear();
+            ColorHist.Series.Add(graphStep);
+            ColorHist.InvalidatePlot(true);
 
             return RGB;
         }
@@ -124,26 +162,81 @@ namespace FuzzyColorHistogram1
                 Lab[index]++;
             }
 
+            // グラフの作成
+            var graphStep = new StairStepSeries()
+            {
+                Color = OxyColors.Red,
+                VerticalLineStyle = LineStyle.None,
+                MarkerType = MarkerType.None
+            };
+
+            for (int x = 0; x < Lab.Count(); x++)
+            {
+                graphStep.Points.Add(new DataPoint(x, Lab[x]));
+            }
+
+            LabHist.Series.Clear();
+            LabHist.Series.Add(graphStep);
+            LabHist.InvalidatePlot(true);
+
             return Lab;
         }
 
         private void CalcHistogram()
         {
-            //createLabHist(getColorData()).ToList<double>().ForEach(x => { Console.WriteLine(x); });
-            //Console.WriteLine("-----------------------------------");
-            Vector<double> rgbHist = createRGBHist(getColorData());
-            
-            FuzzyCMeans fcm = new FuzzyCMeans(createLabHist(getColorData()), 1.9, 64);
+            getColorData(ref points);
 
-            fcm.runFCM();
+            Division = int.Parse(DivisionTextBox.Text);
+
+            rgbHist = createRGBHist(points);
+            labHist = createLabHist(points);
+
+            InfoMationTextBlock.Text = "Load Histogram " + (int)Math.Pow(Division, 3.0) + " bins";
+        }
+
+        private void StartFCH_Click(object sender, RoutedEventArgs e)
+        {
+            double fuzzifier = double.Parse(FuzzifierTextBox.Text);
+            int clusterNum = int.Parse(ClusterNumTextBox.Text);
+
+            FuzzyCMeans fcm = new FuzzyCMeans(labHist, fuzzifier, clusterNum);
+
+            fcm.run();
 
             var U = Matrix<double>.Build.DenseOfArray(fcm.U);
 
-            Vector<double> FCH = Vector<double>.Build.Dense(64);
+            Vector<double> FCH = Vector<double>.Build.Dense(clusterNum);
 
             FCH = U.Transpose() * rgbHist;
 
+            // グラフの作成
+            var graphStep = new StairStepSeries()
+            {
+                Color = OxyColors.Red,
+                VerticalLineStyle = LineStyle.None,
+                MarkerType = MarkerType.None
+            };
+
+            for (int x = 0; x < FCH.Count(); x++)
+            {
+                graphStep.Points.Add(new DataPoint(x, FCH[x]));
+            }
+
+            FCHHist.Series.Clear();
+            FCHHist.Series.Add(graphStep);
+            FCHHist.InvalidatePlot(true);
+
             FCH.ToList().ForEach(x => { Console.WriteLine(x); });
+        }
+
+        public void showToDialogTextBox(string str)
+        {
+            InfoMationTextBlock.Text = str;
+        }
+
+        private void CreateHistogramButton_Click(object sender, RoutedEventArgs e)
+        {
+            CalcHistogram();
         }
     }
 }
